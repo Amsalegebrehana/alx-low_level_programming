e561a7b1cd6f78316b7150102044f3446813bce8
Amsalegebrehana <se.amsale.gebrehana@gmail.com>
2021-06-23 15:22:02 -0400

    1-alphabet done

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ *main function
+ *
+ *print_alphabet - printing the alphabet
+ *
+ */
+void print_alphabet(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+
+}
