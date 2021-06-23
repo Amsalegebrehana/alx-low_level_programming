01d957f9b5a868920be48b10459f118ea56314d8
Amsalegebrehana <se.amsale.gebrehana@gmail.com>
2021-06-23 16:21:13 -0400

    natural

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,18 +1,16 @@
 #include "holberton.h"
 #include <stdio.h>
 /**
- * main - prints first 50 numbers of the fibonacci sequence
- * Return: 0
- */
+* main - prints first 50 numbers of the fibonacci sequence
+* Return: 0
+*/
 int main(void)
 {
 int i, n;
 long f1, f2, next;
-
 f1 = 1;
 f2 = 2;
 n = 50;
-
 for (i = 0; i < n; i++)
 {
 if (i == 49)
