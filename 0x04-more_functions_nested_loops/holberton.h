be6f44efd19e9d18293d0014781a4541dc4d7ca8
Amsalegebrehana <se.amsale.gebrehana@gmail.com>
2021-06-26 00:25:56 -0400

    print numbers 0 to 9 done

--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -3,7 +3,7 @@ int _putchar(char);
 
 /*10 x alphabet */
 void print_alphabet(void);
-
+void print_numbers(void);
 /* 2-print_alphabet_x10.c */
 void print_alphabet_x10(void);
 int mul(int a, int b);
