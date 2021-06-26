ae4ddf15ad83a9d5faa993b53dbc1cae44ab37fc
Amsalegebrehana <se.amsale.gebrehana@gmail.com>
2021-06-26 01:38:28 -0400

    more numers done

--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -5,7 +5,7 @@ int _putchar(char);
 void print_alphabet(void);
 void print_numbers(void);
 void print_most_numbers(void);
-
+void more_numbers(void);
 /* 2-print_alphabet_x10.c */
 void print_alphabet_x10(void);
 int mul(int a, int b);
