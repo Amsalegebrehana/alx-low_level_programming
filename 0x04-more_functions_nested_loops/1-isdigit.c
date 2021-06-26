df0d729b5d81b102b7e7864bcd4cdb9027953dc6
Amsalegebrehana <se.amsale.gebrehana@gmail.com>
2021-06-25 23:53:35 -0400

    1-isdigit done

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,10 +1,10 @@
 #include "holberton.h"
 /**
- * _isdigit - check for number
- * @c : character to check the case
- * Return:0 or 1
- */
+* _isdigit - check for number
+* @c : character to check the case
+* Return:0 or 1
+*/
 int _isdigit(int c)
 {
-return (c>=48 && c<=57);
+return (c >= 48 && c <= 57);
 }
