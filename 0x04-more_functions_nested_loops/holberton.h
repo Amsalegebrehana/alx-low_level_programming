c7df5bd9b7ae35eab5b9ba3134aad62428e6d133
Amsalegebrehana <se.amsale.gebrehana@gmail.com>
2021-06-27 00:41:21 -0400

    printing diagonal done

--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -6,8 +6,12 @@ void print_alphabet(void);
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
+
 /* 2-print_alphabet_x10.c */
 void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
 void print_alphabet_x10(void);
 int mul(int a, int b);
 int _islower(int c);
