d4dc40592102241d4e3865c2569525181703da53
Amsalegebrehana <se.amsale.gebrehana@gmail.com>
2021-06-29 10:56:00 -0400

    string reverse print

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include "2-strlen.c"
+/**
+*print_rev - prints reversed string, followed by a new line
+*@s: pointer to the string to print
+*Return: void
+*/
+void print_rev(char *s)
+{
+int i;
+i = _strlen(s) - 1;
+while (i>=0)
+{
+_putchar(s[i]);
+i--;
+}
+_putchar('\n');
+}
