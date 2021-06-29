11cf4d7b378f2e83d5f8b24532b5b10b4ddb613c
Amsalegebrehana <se.amsale.gebrehana@gmail.com>
2021-06-29 06:04:19 -0400

    change to 98 done

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,10 @@
 #include "holberton.h"
 /**
- *reset_to_98
- *
- *Return: 0
- */
+*reset_to_98 - reset input numbers to 98
+*@n: pointer
+*Return: 0
+*/
 void reset_to_98(int *n)
 {
   *n = 98;
-  
 }
