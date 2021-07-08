7ee2d219a44ca6f4516167479e28ead8bc51be85
Amsalegebrehana <se.amsale.gebrehana@gmail.com>
2021-07-08 01:59:29 -0400

    string lenght

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+*_strlen_recursion - length of a string 
+*@s: pointer of string 
+*Return: int
+*/
+int _strlen_recursion(char *s)
+{
+int n = 0;
+
+if (*s > '\0')
+{
+n += _strlen_recursion(s + 1) + 1;
+}
+return (n);
+}
